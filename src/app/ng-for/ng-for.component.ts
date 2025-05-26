3aba4e4dcf78573cebc2a5983b1803dc0b51b60d
Ponneelan G <ponneelan98@gmail.com>
2025-05-26 14:21:22 +0530

    ngFor

--- src/app/ng-for/ng-for.component.ts
+++ src/app/ng-for/ng-for.component.ts
@@ -1,11 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ng-for',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ng-for.component.html',
   styleUrl: './ng-for.component.css'
 })
 export class NgForComponent {
+  
+  employees:any[]=[
+    {
+      id:1,
+      name:'ponneelan',
+      department:'IT'
+    },
+    {
+      id:2,
+      name:'vishnu',
+      department:'IT'
+    },
+    {
+      id:3,
+      name:'Andal',
+      department:'IT'
+    }
+  ]
+
+  companies:string[]=['zoho','cognizant','wipro','capgemini']
 
 }
