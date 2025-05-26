83ebb45aa488c009ad72b5b0add8d312385ffd3b
Ponneelan G <ponneelan98@gmail.com>
2025-05-26 14:02:42 +0530

    ngIf

--- src/app/ng-if/ng-if.component.ts
+++ src/app/ng-if/ng-if.component.ts
@@ -1,11 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ng-if',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './ng-if.component.html',
   styleUrl: './ng-if.component.css'
 })
 export class NgIfComponent {
+
+  checkBox:boolean=false
+
+  isChecked(){
+    this.checkBox=!this.checkBox
+  }
+
+  inputBox:boolean=true
+
+  onShow(){
+    this.inputBox=true
+  }
+
+  onHide(){
+    this.inputBox=false
+  }
+
+  input1:string=''
+
+  input2:string=''
 
 }
