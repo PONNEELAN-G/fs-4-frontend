52c91ca267d2c727ebd61b73d0bbd60b27f03f12
Ponneelan G <ponneelan98@gmail.com>
2025-05-26 12:05:09 +0530

    first change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { NgIfComponent } from './ng-if/ng-if.component';
+import { NgForComponent } from './ng-for/ng-for.component';
+import { NgSwitchComponent } from './ng-switch/ng-switch.component';
+import { PnfComponent } from './pnf/pnf.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:NgIfComponent},
+    {path:'ngIf',component:NgIfComponent},
+    {path:'ngFor',component:NgForComponent},
+    {path:'ngSwitch',component:NgSwitchComponent},
+    {path:'**',component:PnfComponent}
+];
