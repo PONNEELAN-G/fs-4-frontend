d3dedf07b9ac872f82cdf53e5514a535f27149a6
Ponneelan G <ponneelan98@gmail.com>
2025-05-26 14:43:52 +0530

    ngSwitch

--- src/app/ng-switch/ng-switch.component.ts
+++ src/app/ng-switch/ng-switch.component.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ng-switch',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './ng-switch.component.html',
   styleUrl: './ng-switch.component.css'
 })
 export class NgSwitchComponent {
+
+  grade:number=0
+
+  set(x:number){
+    this.grade=x
+  }
+
+  currentDay:string=''
+
+  days:string[]=['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
 
 }
